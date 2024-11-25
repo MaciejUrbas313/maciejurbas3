801fa95d54ec91e25c0433e735df3590771bc099
Maciej Urbas <maciejurbas313@gmail.com>
2024-11-25 21:28:45 +0100

    :sparkles: Create and import `main-description` component to `main` section

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainMenuComponent } from "./main-menu/main-menu.component";
+import { MainDescriptionComponent } from "./main-description/main-description.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MainMenuComponent],
+  imports: [MainMenuComponent, MainDescriptionComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
