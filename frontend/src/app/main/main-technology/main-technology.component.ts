b2288b2fd2b294c1347cde47bed4cb16efda065d
Maciej Urbas <maciejurbas313@gmail.com>
2024-12-01 13:11:18 +0100

    :sparkles: Generete `main-technology` component

---
+++ frontend/src/app/main/main-technology/main-technology.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-technology',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-technology.component.html',
+  styleUrl: './main-technology.component.css'
+})
+export class MainTechnologyComponent {
+
+}
--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MainMenuComponent } from "./main-menu/main-menu.component";
 import { MainDescriptionComponent } from "./main-description/main-description.component";
+import { MainTechnologyComponent } from './main-technology/main-technology.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MainMenuComponent, MainDescriptionComponent],
+  imports: [MainMenuComponent, MainDescriptionComponent, MainTechnologyComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
