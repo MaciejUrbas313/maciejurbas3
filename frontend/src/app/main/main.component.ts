5a4823c8bb44c2c7e6146da76096045bf77aa555
Maciej Urbas <maciejurbas313@gmail.com>
2025-02-10 09:37:07 +0100

    :sparkles: Create and set `main-about` component
    With responsive CSS.

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MainMenuComponent } from "./main-menu/main-menu.component";
 import { MainDescriptionComponent } from "./main-description/main-description.component";
 import { MainTechnologyComponent } from './main-technology/main-technology.component';
+import { MainAboutComponent } from "./main-about/main-about.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MainMenuComponent, MainDescriptionComponent, MainTechnologyComponent],
+  imports: [MainMenuComponent, MainDescriptionComponent, MainTechnologyComponent, MainAboutComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
